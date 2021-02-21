b52fc2efa464bec2fd497e5fbe048716b529116d
Luke Gammon <lukegammon@gmx.com>
2021-02-21 16:10:41 +0000

    update

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.static(__dirname + '/'));
 
 // Routes
 app.get("/", (req, res) => {
-  res.render('index');
+  res.render('login');
 });
 
 app.get("/users/register", (req, res) => {
